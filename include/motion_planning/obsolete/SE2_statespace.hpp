5b433fb172b260ec6fd0eb2a50cda727d6951ba9
sohail <sohail.e.nia@gmail.com>
2025-08-09 20:01:12 +0330

    Stop tracking obsolete files

--- include/motion_planning/obsolete/SE2_statespace.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2025 Soheil E.nia
-
-#include "motion_planning/state_space/statespace.hpp"
-#pragma once
-
-// class SE2StateSpace: public StateSpace {
-
-// };
--- include/motion_planning/obsolete/grid_node.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 Soheil E.nia
-
-#pragma once
-
-#include "motion_planning/ds/node.hpp"
-
-class GridNode: public Node { 
-
-};
