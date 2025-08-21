3f081d00feb35d13564ec1f66cde195dec772def
sohail <sohail.e.nia@gmail.com>
2025-08-21 18:55:33 +0330

    Remove omp.h

--- include/motion_planning/planners/kinodynamic/kinodynamic_fmtx.hpp
+++ include/motion_planning/planners/kinodynamic/kinodynamic_fmtx.hpp
@@ -6,7 +6,6 @@
 #include "motion_planning/ds/fmt_node.hpp"
 #include "motion_planning/utils/visualization.hpp"
 #include "motion_planning/ds/priority_queue.hpp"
-#include <omp.h> 
 
 class KinodynamicFMTX : public Planner {
  public:
